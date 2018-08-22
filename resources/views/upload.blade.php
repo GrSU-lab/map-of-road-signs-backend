8044f11d0d55dccd73b23827c387fff436d3d237
Zeddikus97 <32907529+Zeddikus97@users.noreply.github.com>
2018-08-22 18:11:29 +0300

    Получил координаты изображения

---
+++ resources/views/upload.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8">
+    <title>Getting photograph geo coordinated in PHP</title>
+</head>
+<body>
+    <h1> <?= $coordinates; ?></h1>
+</body>
+</html>
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Filesystem\Filesystem;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,3 +19,29 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('photo', 'PhotoController@index');
+
+Route::get('/GPS', function () {
+    $exif = exif_read_data(storage_path('app/public/' . 'check.jpg'), 'GPS');
+    $lon = getGps($exif['GPSLongitude'], $exif['GPSLongitudeRef']);
+    $lat = getGps($exif['GPSLatitude'], $exif['GPSLatitudeRef']);
+    $info = $lon.' , '.$lat;
+    return view('upload', ['coordinates' => $info ]);
+});
+
+
+function getGps($exifCoord, $hemi) {
+    $degrees = count($exifCoord) > 0 ? gps2Num($exifCoord[0]) : 0;
+    $minutes = count($exifCoord) > 1 ? gps2Num($exifCoord[1]) : 0;
+    $seconds = count($exifCoord) > 2 ? gps2Num($exifCoord[2]) : 0;
+    $flip = ($hemi == 'W' or $hemi == 'S') ? -1 : 1;
+    return $flip * ($degrees + $minutes / 60 + $seconds / 3600);
+}
+
+function gps2Num($coordPart) {
+    $parts = explode('/', $coordPart);
+    if (count($parts) <= 0)
+        return 0;
+    if (count($parts) == 1)
+        return $parts[0];
+    return floatval($parts[0]) / floatval($parts[1]);
+}
