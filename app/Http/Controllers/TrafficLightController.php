5af8218280f5a803c0b6ca5f0512c1be8f3c4789
Mozder <rekeloz@gmail.com>
2018-08-26 07:01:05 -0700

    Добавил модель, контроллер, миграцию

---
+++ app/Http/Controllers/TrafficLightController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TrafficLightController extends Controller
+{
+    //
+}
--- database/migrations/2018_08_26_135105_create_traffic_lights_table.php
+++ database/migrations/2018_08_26_135105_create_traffic_lights_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordResetsTable extends Migration
+class CreateTrafficLightsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,12 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('traffic_lights', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('image_path');
+            $table->float('lat');
+            $table->float('long');
+            $table->timestamps();
         });
     }
 
@@ -27,6 +29,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('traffic_lights');
     }
 }
